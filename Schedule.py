176b5eaab4c6d7888865db7e8b2daac8eb699241
rondreas <andreas.ranman@gmail.com>
2016-03-08 22:57:45 +0100

    Removed bloat and clutter
    The Schedule class used datetime.strf() to convert datetimes into
    string, which I then later only used to convert back into datetimes for
    comparision. Seemed confused and terrible so I removed it.
    
    This allowed to cut away all the conversions from strings to actual
    datetimes in main.py for a cleaner look.

--- Schedule.py
+++ Schedule.py
@@ -2,14 +2,9 @@ import icalendar
 import requests
 from datetime import datetime
 
-#TODO evaluate removal of getNextEventID() and getEvent(), also the removal of
-# adding a id to stored events.
-
-#TODO Why convert event values to string? When they are mostly converted back
-# to datetime objects for operations.
-
 class Schedule():
-    ''' Create a schedule object, holding the events from doodle ical schedule taken from given url.'''
+    ''' Create a schedule object, holding the events from doodle ical schedule 
+    taken from given url.'''
     def __init__(self, url):
         try:
             self.request = requests.get(url)
@@ -17,15 +12,17 @@ class Schedule():
             print("Error: Could not get requested URL")
 
         self.events = []
-        self.tzDeltaTime = datetime.now() - datetime.utcnow()
         self.calendar = icalendar.Calendar.from_ical(self.request.content)
 
         if self.request:
             self.parseCalendar()
 
     def parseCalendar(self):
+        ''' Goes through the Doodle ical url, ignoring already past events
+        and stores the information for each event as a dictionary. Each
+        dictionary will then be appended to class list of events. '''
 
-        sid = 0
+        tzDeltaTime = datetime.now() - datetime.utcnow()
 
         for event in self.calendar.walk('vevent'):
             summary = event.get('summary')
@@ -34,48 +31,24 @@ class Schedule():
 
             # Doodle datetimes are represented in gmt so I'm adjusting
             # the datetime objects to represent local time.
-            dtStart.dt += self.tzDeltaTime
-            dtEnd.dt += self.tzDeltaTime
+            dtStart.dt += tzDeltaTime
+            dtEnd.dt += tzDeltaTime
 
             # One can't compare naive to aware datetime objects, so we
             # strip the tzinfo from the calendar datetime.
             if dtEnd.dt.replace(tzinfo=None) > datetime.now():
-                self.events.append({'summary':str(summary),
-                                    'date':dtStart.dt.replace(tzinfo=None).date().strftime('%Y-%m-%d'),
-                                    'timeStart':dtStart.dt.replace(tzinfo=None).time().strftime('%H:%M'),
-                                    'timeEnd':dtEnd.dt.replace(tzinfo=None).time().strftime('%H:%M'),
-                                    'id':sid,
-                                    'datetime':dtEnd.dt.replace(tzinfo=None)})
-
-            # increment shift id number
-            sid += 1
-
-    def getNextEventID(self):
-        ''' Get the next scheduled shift ID. '''
-
-        earliest = ''
-        earliest_id = None
-        dtPattern = "%Y-%m-%d %H:%M"
-
-        for event in self.events:
-            check = event['date'] + ' ' + event['timeStart']
-            # if earliest is empty...
-            if not earliest:
-                earliest = check
-                earliest_id = event['id']
-            # Compare datetimes for which one is earliest.
-            elif datetime.strptime(earliest, dtPattern) > datetime.strptime(check, dtPattern):
-                earliest = check
-                earliest_id = event['id']
-
-        return earliest_id
+                self.events.append({'summary':summary,
+                                    'timeStart':dtStart.dt.replace(tzinfo=None),
+                                    'timeEnd':dtEnd.dt.replace(tzinfo=None)})
 
     def sortEventsByDatetime(self):
-        ''' '''
-        newList = sorted(self.events, key=lambda k: k['datetime'])
+        ''' Takes the stored events list and returns a list sorted in
+        chronological ascending order. '''
+        newList = sorted(self.events, key=lambda k: k['timeEnd'])
         return newList
 
     def getRoom(self, event):
+        ''' Checks which room given event is to take place in '''
         if 'zombie' in event['summary'].lower():
             return 'zombie_lab'
         elif 'bank' in event['summary'].lower():
@@ -85,15 +58,10 @@ class Schedule():
         else:
             print("Unexpected event, nothing to return")
 
-    def getEvent(self, sid):
-        for event in self.events:
-            if event['id'] == sid:
-                return event
-
     def prunePastEvents(self):
         ''' Goes through our list of events and deletes any event
         that has already passed. '''
-        self.events[:] = [event for event in self.events if event['datetime']>datetime.now()]
+        self.events[:] = [event for event in self.events if event['timeEnd']>datetime.now()]
 
 if __name__ == '__main__':
     url = ''
--- main.py
+++ main.py
@@ -28,20 +28,9 @@ def main(schedule_url):
         rooms.append(currentRoom)
         rooms.append(nextRoom)
 
-        # Get datetimes for start and finish of shifts.
-        e1Start = schedule.events[0]['date'] + ' ' + schedule.events[0]['timeStart']
-        e1Start = datetime.strptime(e1Start, "%Y-%m-%d %H:%M")
-        e2Start = schedule.events[1]['date'] + ' ' + schedule.events[1]['timeStart']
-        e2Start = datetime.strptime(e2Start, "%Y-%m-%d %H:%M")
-
-        e1End = schedule.events[0]['date'] + ' ' + schedule.events[0]['timeEnd']
-        e1End = datetime.strptime(e1End, "%Y-%m-%d %H:%M")
-        e2End = schedule.events[1]['date'] + ' ' + schedule.events[1]['timeEnd']
-        e2End = datetime.strptime(e2End, "%Y-%m-%d %H:%M")
-
         # Find all games during focused shifts
-        games1 = rooms[0].findGames(e1Start, e1End)
-        games2 = rooms[1].findGames(e2Start, e2End)
+        games1 = rooms[0].findGames(schedule.events[0]['timeStart'], schedule.events[0]['timeEnd'])
+        games2 = rooms[1].findGames(schedule.events[1]['timeStart'], schedule.events[1]['timeEnd'])
 
         games1 = rooms[0].filter(games1, Status='Reserved')
         games2 = rooms[1].filter(games2, Status='Reserved')
